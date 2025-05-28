9b25bf6cb27ca4c222da0e3b40a95ff94e6336ba
Lucas Fernandes Nunes Oliveira <lucasfernandesnunesoliveira@gmail.com>
2025-05-28 12:31:58 -0300

    Primeiro commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function validarFormulario() {
+  const nome = document.getElementById('nome').value;
+  const email = document.getElementById('email').value;
+  const mensagem = document.getElementById('mensagem').value;
+
+  if (!nome || !email || !mensagem) {
+    alert("Por favor, preencha todos os campos.");
+    return false;
+  }
+
+  alert("Mensagem enviada com sucesso!");
+  return true;
+}
